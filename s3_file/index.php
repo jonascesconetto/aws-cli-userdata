4c7fc65671b48f703e8551d461ca12fe2c08d3b8
Jonas Cesconetto <jonascesconetto@gmail.com>
2022-06-07 18:07:38 -0300

    First commit

---
+++ s3_file/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$url = "http://169.254.169.254/latest/meta-data/instance-id";
+$instance_id = file_get_contents($url);
+echo "<h1?> <font color='green'> Instance ID: <b>" . $instance_id . "</b> <br/> </font> </h1>"
+
+?>
